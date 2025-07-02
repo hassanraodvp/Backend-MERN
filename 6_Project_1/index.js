1293f549634e079dbcb31258b86837abcbe1c076
Hassan-Akhtyr <hassanakhtyr@gmail.com>
2025-07-02 13:41:52 +0500

    implement task editing functionality and update UI components

--- 6_Project_1/index.js
+++ 6_Project_1/index.js
@@ -11,18 +11,42 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   fs.readdir("./tasks", (err, tasks) => {
     res.render("index", { task: tasks });
-    console.log(tasks)
   });
-})
+});
 app.get("/tasks/:filename", (req, res) => {
-  fs.readFile(`./tasks/${req.params.filename}`, "utf-8" ,(err, data) => {
-    res.render("show", { filename: req.params.filename, data:  data });
-  })
-})
-app.post("/create", (req, res) => {
-  fs.writeFile(`./tasks/${req.body.title.split(' ').join('')}.txt`, req.body.msg, (err) => {
-    res.redirect("/");
+  fs.readFile(`./tasks/${req.params.filename}`, "utf-8", (err, data) => {
+    res.render("show", { filename: req.params.filename, data: data });
   });
+});
+app.get("/edit/:filename", (req, res) => {
+  fs.readFile(`./tasks/${req.params.filename}`, "utf-8", (err, data) => {
+    res.render("edit", { filename: req.params.filename, data });
+  });
+});
+app.post("/edit", (req, res) => {
+  const oldPath = `./tasks/${req.body.title}`;
+  const newPath = `./tasks/${req.body.update_title}.txt`;
+  const newMsg = req.body.msg;
+
+  // First rename (if needed), then write updated content
+  fs.rename(oldPath, newPath, (err) => {
+    if (err) return res.status(500).send("Rename failed");
+
+    fs.writeFile(newPath, newMsg, "utf-8", (err) => {
+      if (err) return res.status(500).send("Write failed");
+      res.redirect("/");
+    });
+  });
+});
+
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./tasks/${req.body.title.split(" ").join("")}.txt`,
+    req.body.msg,
+    (err) => {
+      res.redirect("/");
+    }
+  );
 });
 app.listen(port, () => {
   console.log(`App listening at  ${port}`);
